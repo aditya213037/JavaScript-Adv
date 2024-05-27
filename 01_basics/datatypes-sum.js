bb3773e8f399c84e89d68a4367a8d2c5758cf91d
Aditya Gupta <aditya2113037@akgec.ac.in>
2024-05-27 16:11:27 +0000

    stack&heap memory

--- 01_basics/datatypes-sum.js
+++ 01_basics/datatypes-sum.js
@@ -41,3 +41,38 @@
    console.log(typeof myFunction);   //function
    console.log(typeof heroes);       //object
    console.log(typeof accountID)     //symbol
+
+   /* ------------------------------------------------------------------------------- */
+
+   /*
+   
+                                        MEMORY
+                                     _____|______
+                                    |            |
+                                  STACK         HEAP
+                              (Primitive)    (Non-Primitive)
+                        -> Call by value      -> Call by Rference
+
+                        -> changes occurs      -> Changes occur in 
+                           in copy of var        original value of var
+
+
+*/
+
+let myProjectone = "Digital Marketing";
+let myProjecttwo = myProjectone;
+myProjecttwo = "Online cafe";   ///  Changes occurs only on created copy not on original value.
+
+console.log(myProjectone);
+console.log(myProjecttwo);
+
+let objone = {
+    email : "ag@gmail.com",
+    upi : "adi@ybl"
+}
+
+let objtwo = objone;
+ objtwo.email = "aditya1122@gmail.com";
+
+console.log(objone);
+console.log(objtwo);
