f557a7e5a30cdce5e12dce823b85b907f8d0b882
Aditya Gupta <aditya2113037@akgec.ac.in>
2024-05-30 10:55:03 +0000

    while loop

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,19 @@
+/* ----------- WHILE Loop --------- */
+let i=0
+while (i<=10) {
+    // console.log(`Value is ${i}`);
+    i++;
+}
+
+const arr1= ["shaktiman","Krrish","Ra.One"];
+let index=0;
+while (index < arr1.length) {
+    // console.log(`Value is : ${arr1[index]}`);
+    index++;
+}
+
+let j=0;
+do {
+    console.log(`value are : ${j}`);
+    j=j+2;
+} while (j<=10);
